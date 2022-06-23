eb1d05374a33021c27971952dd471091eedccc65
ImMyst <titouan.galvani@yeap.ai>
2022-06-23 20:49:07 +0200

    feat: remove lorem ipsum

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,10 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
     return (
         <div className="flex w-full flex-1 flex-col items-center justify-center space-y-6 px-20 text-center">
-            <h1 className="max-w-2xl text-3xl font-bold leading-10">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, porro?
-            </h1>
-            <div className="max-w-xl leading-7">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nesciunt reiciendis eligendi obcaecati
-                possimus ex minus, non eius porro quam hic nam enim, facilis iste libero accusamus animi?
+            <h1 className="max-w-2xl text-5xl font-bold">Regroupe tes citations préférées au même endroit.</h1>
+            <div className="max-w-xl text-lg font-light leading-7 text-gray-400">
+                kuotes te permet d'ajouter des citations et des messages et de les partager avec tout le monde. Tu es maître
+                de tes citations, et tu peux les partager avec tes amis pour en faire profiter tout le monde.
             </div>
             <div className="flex justify-center">
                 <KButton url={'/signup'}>{'Commencer'}</KButton>
